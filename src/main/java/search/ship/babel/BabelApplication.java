6cb868db20b16e66abd3b2e35fff324bc601859d
riverrevir <81510864+riverrevir@users.noreply.github.com>
2022-06-07 01:10:58 +0900

    init

---
+++ src/main/java/search/ship/babel/BabelApplication.java
@@ -0,0 +1,13 @@
+package search.ship.babel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/search/ship/babel/BabelApplicationTests.java
@@ -0,0 +1,13 @@
+package search.ship.babel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
