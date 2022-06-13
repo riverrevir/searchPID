98b960a522e49cfcceb74c324eefff2596cdf501
DESKTOP-G3USBLG\simba <simba0502@naver.com>
2022-06-14 02:05:28 +0900

    [feat] IMG 파일 다운로드 API 작성

---
+++ src/main/java/search/ship/babel/dto/FileDownloadRequest.java
@@ -0,0 +1,11 @@
+package search.ship.babel.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FileDownloadRequest {
+    private String imgName;
+    private String imgType;
+}
