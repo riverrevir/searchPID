5dc6574d56dc8e561e63ecb687c69a8d6b3b56d7
riverrevir <81510864+riverrevir@users.noreply.github.com>
2022-06-15 01:11:57 +0900

    fixed: param error fixed

--- src/main/java/search/ship/babel/repository/ShipRepository.java
+++ src/main/java/search/ship/babel/repository/ShipRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import search.ship.babel.domain.Ship;
 
@@ -16,5 +17,5 @@ public interface ShipRepository extends JpaRepository<Ship, Long> {
             countQuery = "SELECT count(s) FROM ship s" +
                     " WHERE s.project = :project"
     )
-    Page<Ship> findAllByProject(String project, Pageable pageable);
+    Page<Ship> findAllByProject(@Param("project") String project, Pageable pageable);
 }
