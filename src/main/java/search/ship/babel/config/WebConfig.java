1a92859aad65d2977ffddc96a848cfdb904bc95b
riverrevir <81510864+riverrevir@users.noreply.github.com>
2022-07-12 15:32:17 +0900

    fixed: post 요청 개방

--- src/main/java/search/ship/babel/config/WebConfig.java
+++ src/main/java/search/ship/babel/config/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("OPTIONS","GET");
+                .allowedMethods("OPTIONS","GET","POST");
     }
 }
